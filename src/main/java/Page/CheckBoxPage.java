06719d646d808dc66c0ab76e492e38eb84f4d451
intgrity-rd-auto <subh078@yahoo.in>
2020-02-25 16:36:47 +0530

    pom.xml modification

--- src/main/java/Page/CheckBoxPage.java
+++ src/main/java/Page/CheckBoxPage.java
@@ -10,9 +10,9 @@ import Base.TestBase;
 public class CheckBoxPage extends TestBase {
 
 	@FindBy(xpath = "//h2/input[\"@type('checkbox')\"]")
-	public	static	WebElement Store_btn;
+	public static WebElement Store_btn;
 	@FindBy(xpath = "//h4[contains(text(),'Boxcutter')]/input")
-	public	static	WebElement BoxCutter_btn;
+	public static WebElement BoxCutter_btn;
 
 	public CheckBoxPage() {
 		PageFactory.initElements(driver, this);
@@ -21,14 +21,12 @@ public class CheckBoxPage extends TestBase {
 	public static void clickStoreBtn() {
 
 		Store_btn.click();
-		
+
 	}
-	
+
 	public static void boxCutterBtn() {
 
 		BoxCutter_btn.click();
 	}
-	
-	
 
 }
